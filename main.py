7b0cba2681dd9c80084238e6a9343349bd715700
Miguel Lopez Ariza <lopezarizamiguel@gmail.com>
2024-09-20 20:50:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class User(BaseModel):
+    email: str
+    password: str
+
+
 
 @app.get("/")
 def read_root():
@@ -16,6 +32,10 @@ def list_users():
         {
            'name': 'Leo Lopez Ariza',
            'email': 'lopezarizaleo@gmail.com' 
+        },
+        {
+           'name': 'Alfredo Lopez M',
+           'email': 'alfredo@gmail.com' 
         }
     ]
 
@@ -29,3 +49,20 @@ def list_products():
            'name': 'Arina',
         }
     ]
+
+
+@app.post("/login")
+def login(dato: User):
+    if (dato.email == 'miguel@test.com' and dato.password == '1234'):
+        return {
+            'status': 'success',
+            'message': 'Datos correctos',
+            'data': {
+                'user_id': 1
+            }
+        }
+    
+    return {
+        'status': 'error',
+        'message': 'Si yaaaa!'
+    }
